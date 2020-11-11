ff40d4d6ba33b7bb99f1bd6a0951e19ad6737fcd
Janusz Pasternak <januszpasternak@MacBook-Air-Maria.local>
2020-11-11 13:57:24 +0100

    6.2 What is Test Driven Development (TDD)?

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+        int a = 4;
+        int b = 2;
+
+        int resultAdd = calculator.add(a, b);
+        int resultSub = calculator.subtract(a, b);
+
+        System.out.println("Pierwszy test: ");
+        if (resultAdd == a + b) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        System.out.println("\nDrugi test: ");
+        if (resultSub == a - b) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
     }
 }
