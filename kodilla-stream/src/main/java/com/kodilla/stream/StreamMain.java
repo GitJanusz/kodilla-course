2099a4b5ef337732124af5a18226d48dcb4161b3
Janusz Pasternak <januszpasternak@MacBook-Air-Maria.local>
2020-11-25 22:18:56 +0100

    7.3 Functions stream(), filter(), map() and collect() - korekta

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,21 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.Book;
-import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
-import com.kodilla.stream.person.People;
 
-import java.util.List;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
+
         Forum theForum = new Forum();
         Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
                 .filter(user -> user.getSex() == 'M')
-                .filter(user -> user.getBirthDate().getYear() <= 2000)
+                .filter(user -> Period.between(user.getBirthDate(), LocalDate.now()).getYears() >= 20)
                 .filter(user -> user.getPostsCountPublished() >= 1)
                 .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
 
