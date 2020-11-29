712f4b6b92c15c834d4d23dc6e61e36cfd2d0a0e
Janusz Pasternak <januszpasternak@MacBook-Air-Maria.local>
2020-11-29 14:04:56 +0100

    7.5 information about IntStream - zadanie

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.stream.array;
+
+import org.junit.Test;
+
+import java.util.OptionalDouble;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] number = new int[20];
+        number[0] = 6;
+        number[1] = 5;
+        number[2] = 4;
+        number[3] = 7;
+        number[4] = 9;
+        number[5] = 2;
+        number[6] = 3;
+        number[7] = 12;
+        number[8] = 1;
+        number[9] = 6;
+        number[10] = 13;
+        number[11] = 7;
+        number[12] = 8;
+        number[13] = 3;
+        number[14] = 4;
+        number[15] = 6;
+        number[16] = 2;
+        number[17] = 9;
+        number[18] = 1;
+        number[19] = 3;
+
+        //When
+        OptionalDouble arrayAverage = ArrayOperations.getAverage(number);
+
+        //Then
+        OptionalDouble expected = OptionalDouble.of(5.55);
+        assertEquals(expected, arrayAverage);
+    }
+}
