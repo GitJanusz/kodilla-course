cd005d74bb9de7ca28caa932334dbc0e3099332f
Janusz Pasternak <januszpasternak@MacBook-Air-Maria.local>
2021-01-22 16:29:47 +0100

    14.2 Zadanie: kalkulator z wyświetlaczem

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class Display {
     public double displayValue(double val) {
+        System.out.println(val);
         return val;
     }
 }
